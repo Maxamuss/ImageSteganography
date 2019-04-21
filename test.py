e87cde7b611c9fb306a444b468a58c1459112448
revisics <maxwilkinson99@gmail.com>
2019-04-21 15:54:56 +0100

    hide_data function complete. Missing the adding of the meta data

--- test.py
+++ test.py
@@ -3,17 +3,27 @@ from PIL import Image
 class HiddenImage:
     """
     Class used for hidding or retriving hidden data from an image.
+    Each pixel in the image can store 6 bits of data. 4 pixels are grouped in
+    order to store 3 characters. Pixels are grouped horizontally, starting at
+    the top left of the image.
     """
     def __init__(self, image_name):
         image = Image.open('1.jpg')
         pixels = image.load()
 
-        self.image_name = image_name
         self.image = image
         self.raw_pixel_store = pixels
+        self.image_name = image_name
         self.x_size = image.size[0]
         self.y_size = image.size[1]
-        self.hidden_data = self.has_data_hidden()
+
+    def get_character_capacity(self):
+        """
+        Return the number of characters that can be stored in the image.
+        """
+        pixels = (self.x_size * self.y_size) - 60
+        characters = pixels * 0.75
+        return characters
 
     def has_data_hidden(self):
         """
@@ -23,9 +33,12 @@ class HiddenImage:
         data hidden in it.
         """
         # get the last 60 pixels of the image
-        pixels_rgb = []
-        for i in range(self.x_size-1, self.x_size-61, -1):
-            pixels.append(self.raw_pixel_store[i, self.y_size-1])
+        if self.x_size >= 60:
+            pixels_rgb = []
+            for i in range(self.x_size-1, self.x_size-61, -1):
+                pixels.append(self.raw_pixel_store[i, self.y_size-1])
+        else: #TODO: if image width less than 60 px
+            return False
 
         pixels_binary = []
         for pixel in pixels_rgb:
@@ -33,7 +46,7 @@ class HiddenImage:
 
         return False
 
-    def add_hidden_data(self, data, fit_all=True):
+    def hide_data(self, data, fit_all=True):
         """
         Hide data in the image. By default, if the data to be hidden will not
         fit into the image, none of the data is hidden and False returned. If
@@ -48,27 +61,84 @@ class HiddenImage:
         """
         data = str(data)
         # check data will fit into the image (60 px reserved for meta data)
-        image_pixels = self.x - self.y - 60
-        size_available = int(image_pixels / 4)
         data_size = len(data)
+        size_available = self.get_character_capacity()
 
-        if fit_all and data_size > available_pixels:
+        if fit_all and data_size > size_available:
             return False
 
+        # convert the data to a string of binary
         binary_data = self.data_to_binary(data)
+
+        # split the binary into groups of 6 digits and add data to image
+        x = 0
+        y = 0
+        while (binary_data != ''):
+            temp_data = binary_data[:6]
+            binary_data = binary_data[6:]
+            self.set_pixel_value(x, y, temp_data)
+
+            if x == self.x_size:
+                x = 0
+                y += 1
+            else:
+                x += 1
+
+        self.image.save('test.jpg', 'JPEG')
+        # add the meta data to the end
+        # self.add_meta_data(self)
 
     def data_to_binary(self, data):
         """
         Converts the given string of data to a binary. Each character in the
-        string is converted to binary and added to a list, to be returned
+        string is converted to binary and added to a list. A string is returned.
         """
-        # for char in data:
-        pass
+        # convert the string to ascii values then binary
+        data_ascii = [ord(c) for c in data]
+        data_binary = [format(a, '08b') for a in data_ascii]
 
-    def rgb_to_binary(self, rgb):
+        binary_string = ''
+        for byte in data_binary:
+            binary_string += byte
+
+        return binary_string
+
+    def rgb_to_binary(self, rgb_list):
         """
-        Convery
+        Returns the a list of the rgb values of the pixel in binary given a
+        tuple of rgb colours.
         """
+        colours_binary = []
+        for colour in rgb_list:
+            colours_binary.append(format(colour, '08b'))
+
+        return colours_binary
+
+    def set_pixel_value(self, x, y, data):
+        """
+        Change the binary rgb values of a pixel to store the given data.
+        """
+        pixel = self.raw_pixel_store[x, y]
+        pixel_binary = self.rgb_to_binary(pixel)
+
+        r = pixel_binary[0]
+        g = pixel_binary[1]
+        b = pixel_binary[2]
+        # change the last 2 digits of the binary number and convert to an int
+        r_adj = int((r[:6] + data[:2]), 2)
+        g_adj = int((g[:6] + data[2:4]), 2)
+        b_adj = int((b[:6] + data[4:6]), 2)
+        # set the next rgb value for the pixel
+        self.raw_pixel_store[x, y] = (r_adj, g_adj, b_adj)
+
+    def add_meta_data(self):
+        """
+        Add some infomation to the image about the data that has been hidden
+        inside of it. Uses the last 60 pixels of the image, starting at the
+        bottom right pixel and moving horizontally left.
+        """
+        # for i in range(self.x_size, ):
+        pass
 
     def retrive_hidden_data(self):
         """
@@ -84,55 +154,11 @@ class HiddenImage:
             if check != 'y':
                 return None
 
-
-class Pixel:
-    """
-    Class used for storing data of individual pixels from an image. Only created
-    for pixels that are needed for the steganography.
-    """
-    def __init__(self, x, y, rgb):
-        """
-        Set the x and y co-ordinates of the pixel in the image as well as its
-        rgb colour value.
-        """
-        self.x = x
-        self.y = y
-        self.rgb = rgb
-
-    def to_binary(self):
-        """
-        Convert the pixel's rgb values to binary. Returns a tuple of the binary
-        values for the red, green and blue values in binary.
-        """
-        r = self.rgb[0]
-        g = self.rgb[1]
-        b = self.rgb[2]
-
-        r_binary = int(format(r, 'b'))
-        g_binary = int(format(g, 'b'))
-        b_binary = int(format(b, 'b'))
-
-        rgb_binary = (r_binary, g_binary, b_binary)
-
-        return rgb_binary
-
-    def get_next(self):
-        """
-        Returns a Pixel object of the pixel to the right of self. If no pixel is
-        to the right, return the Pixel object of the first pixel on the next row
-        down. None will be returned if this is the last pixel in the image.
-        """
-        pass
-
-    def get_prev(self):
-        """
-        Returns a Pixel object of the pixel to the left of self. If no pixel is
-        to the left, return the Pixel object of the last pixel on the row above.
-        None will be returned uf this is the first pixel in the image.
-        """
-        pass
+    def return_colour(self, x, y):
+        return self.raw_pixel_store[x, y]
 
 image = HiddenImage('1.jpg')
+image.hide_data("My name is Max Wilkinson.")
 
 # print(pixel.to_binary())
 # im = Image.open(
